f47b3f839ba7390e0dcdb15dac03b2add9dad377
bobir01 <68824641+bobir01@users.noreply.github.com>
2021-12-20 19:20:47 +0500

    .

--- gsheets_test_tg/handlers/users/start.py
+++ gsheets_test_tg/handlers/users/start.py
@@ -2,7 +2,9 @@ from os import stat
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 from aiogram.dispatcher import FSMContext
-from loader import dp
+from aiogram.types import chat
+from data.config import ADMINS
+from loader import dp, bot
 
 
 @dp.message_handler(CommandStart())
@@ -10,3 +12,5 @@ async def bot_start(message: types.Message, state: FSMContext):
     await message.answer(f"Hi , {message.from_user.full_name}! send me your student \
 id and I will tell you all information about your attendece of precalculus")
     await state.set_state("student_id")
+
+    await bot.send_message(chat_id=ADMINS[0], text=f"{message.from_user.get_mention()}")
